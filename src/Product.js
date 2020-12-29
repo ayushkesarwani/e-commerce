72b1576f0b9a59042de1b8b289d954dd92dba1bf
ayushkesarwani786 <ayushkesarwani786@gmail.com>
2020-12-30 01:05:56 +0530

    third push

--- src/Product.js
+++ src/Product.js
@@ -1,27 +1,26 @@
 import React, {useState,useEffect} from 'react'
 import "./product.css";
-import ProductDesc from "./ProductDesc";
-;
 function Product({image, name, price}) {
 
     const [productImage, setProductImage] = useState("");
     const [productName, setProductName] = useState("");
     const [productPrice, setProductPrice]=useState("");
-
+ 
     
 
     return (
         <div className="product">
             
-
             {/* Image */}
             <img
                 className="product__image" 
                 src={image}
                 alt="product_image"
             />
+
             {/* name */}
             <h3> {name} </h3>
+
             {/* price */}
             <h3>{price}</h3> 
 
--- src/ProductDesc.js
+++ src/ProductDesc.js
@@ -1,19 +1,57 @@
-import React from 'react'
-import ReactModal from 'react-modal';
+import React, {useState, useEffect} from 'react'
+import "./productDesc.css"
 
-function ProductDesc() {
-    console.log("Product Description me a gya....")
+const ProductDesc = ({productArray, prodId}) => {
+
+    const [prodDisplay, setProdDisplay] = useState({})    
+    
+    useEffect(() => {
+        
+        
+            productArray.map((product) => {
+                prodId === product.id ? (
+                    
+                            //console.log("object initialized")
+                            setProdDisplay(product)    
+                    
+                ) : 
+                (
+                    console.log("not ok")
+                )
+            })
+        }
+    , [])
+
+    
+    
     return (
         <div className="productDesc">
-            <h1> Hello WOrld</h1>
-            <ReactModal
-                isOpen={true}
-            >
-                Description is here!
-            </ReactModal>
+            {/* <h1>Now Description is here...</h1> */}
+            {/* <h1>{prodId}</h1> */}
 
+            {console.log(prodDisplay)}
+            
+            
+            {/* <div>{prodDisplay.name} </div> */}
+
+            <div className="productDesc__image">
+                <img 
+                    src={prodDisplay.image}
+                    alt="Image not found"
+                />
+            </div>
+            
+            <div className="productDesc__info">
+                <h1>{prodDisplay.name}</h1>
+
+                <p>{prodDisplay.description}</p>
+
+                <h3><strong>{prodDisplay.price}</strong></h3>
+            </div>
+
+                
+                
         </div>
     )
 }
-
-export default ProductDesc
+export default ProductDesc;
